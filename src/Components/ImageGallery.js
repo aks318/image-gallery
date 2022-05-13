f87eae841ce8a1d23f345dd97847cf0549e87cf6
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2022-05-14 02:05:53 +0530

    Image diaplyed into gallery

--- src/Components/ImageGallery.js
+++ src/Components/ImageGallery.js
@@ -1,18 +1,44 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Context as ImageContext} from '../Context/ImageContext'
+import './ImageGallery.css'
 
 const ImageGallery = () => {
     const{
         state : ImageState,
         fetchImage
     } = useContext(ImageContext)
+    const [imageList , setImageList] = useState([])
 
     useEffect(() => {
         fetchImage("5ecb5c353000008f00ddd5a0")
     }, [])
+
+    useEffect(() => {
+      if(ImageState.image.length > 0) setImageList(ImageState.image)
+    }, [ImageState.image])
     
   return (
-    <div>ImageGallery</div>
+    <div className='gallery'>
+      {imageList.map((image , index) => 
+        {
+          if(index !== 0){
+            return (<LazyLoadImage
+            key={index}
+            effect='blur'
+            alt={`Image${index}`}
+            src={image.urls.full} />)
+          }
+          else{                             // First image is giving 404 error
+            return (<LazyLoadImage
+              key={index}
+              effect='blur'
+              alt={`Image${index}`}
+              src={imageList[5].urls.full} />)
+          }
+        }
+      )}
+    </div>
   )
 }
 
